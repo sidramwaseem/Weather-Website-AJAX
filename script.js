86ef2b9eb2acac4ec7832a6630a75973e51513df
sidramwaseem <sidraw24@gmail.com>
2022-03-19 02:16:48 +0500

    changes

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-$(document).ready(function() {
-  
-})
+$(document).ready(function () {
+  $("#searchInput").on("keyup", function (e) {
+    var cityName = e.target.value;
+    var API = "c7a9e78687d4f87bb32878918bfd6a1e";
+
+    //Make the request to the server
+
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}`,
+    }).done(function (weatherData) {
+      console.log(weatherData);
+
+      $("#profile").html(`
+           <div class="row">
+             <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                 <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+        `);
+    });
+  });
+});
